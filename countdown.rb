00f81d2e0573a6254377de48f04efa2cb2a2e1c8
Erin Green <erin.y.green@gmail.com>
2018-09-18 17:26:02 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,11 +4,12 @@
 
 def countdown(number)
   number == 10 
-  while number > 0 
-  return "#{number} SECOND(S)!"
-  sleep(1)
-  number -= 1 
-end 
-
+  def countdown_with_sleep
+    while number > 0 
+    puts "#{number} SECOND(S)!"
+    sleep(1)
+    number -= 1 
+    end 
+  end 
 return  "HAPPY NEW YEAR!"
 end
