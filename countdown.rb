f7064126c9c13b4206a1b7c0eff67ddd4d99f7da
Erin Green <erin.y.green@gmail.com>
2018-09-18 17:26:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,7 +6,7 @@ def countdown(number)
   number == 10 
   def countdown_with_sleep
     while number > 0 
-    puts "#{number} SECOND(S)!"
+    return "#{number} SECOND(S)!"
     sleep(1)
     number -= 1 
     end 
